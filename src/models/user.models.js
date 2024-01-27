911286551bb0212b35ee759e794401ac291a4693
xahmad-ali <ahmadhashmi152003@gmail.com>
2024-01-27 08:37:23 -0800

    tokens and bcrypt and jsonwebtoken

---
+++ src/models/user.models.js
@@ -0,0 +1,126 @@
+import mongoose, { Schema } from "mongoose";
+import jsonwebtoken from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+
+    username: {
+        type: String,
+        required : true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required : true,
+        unique: true,
+        trim: true
+    },
+    fullname: {
+        type: String,
+        required : true,
+        unique: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String, // url
+        required: true  
+    },
+    coverImage: {
+        type: String
+    },
+    watchHistory: {
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    },
+    password:{
+        type: String,
+        required: [true, 'Password is rquired']
+    },
+    refreshToken: {
+        type: String
+    }
+
+
+},
+{
+    timestamps: true
+}
+
+)
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+   return await bcrypt.compare(password, this.password)
+}
+
+
+
+
+userSchema.methods.generateAccessToken = function(){
+    return jsonwebtoken.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    );
+}
+
+userSchema.methods.generateRefreshToken = function() {
+    return jsonwebtoken.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    );
+}
+
+export const User = mongoose.model("User", userSchema);
+
+/*
+userSchema.methods.generateAccessToken = function(){
+    jsonwebtoken.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+)}
+
+userSchema.methods.generateRefreshToken = function() {
+    jsonwebtoken.sign(
+        {
+            _id: this._id
+
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn:REFRESH_TOKEN_EXPIRY }
+    )
+}
+
+
+
+
+export const User = mongoose.model("User", userSchema)
+*/
