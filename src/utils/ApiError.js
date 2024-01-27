71e9f17117ff7fb33b0baa2c067ef6e2aff67655
xahmad-ali <ahmadhashmi152003@gmail.com>
2024-01-27 06:42:25 -0800

    mistake = instead of :

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -2,7 +2,7 @@ class ApiError extends Error {
 
     constructor(
         statusCode,
-        message: "Something went wrong",
+        message= "Something went wrong",
         errors = [],
         statck = ""
 
